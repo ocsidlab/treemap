87bb5c949449b761c38d2146ba0956961bb795a5
Arun Ganesh <arun.planemad@gmail.com>
2016-06-04 09:00:07 +0530

    Missing brace

--- index.js
+++ index.js
@@ -43,157 +43,159 @@ var overlayData = {
 
 // Map ready
 map.on('style.load', function(e) {
-            init();
+    init();
 
 
-            function init() {
+    function init() {
 
-                map.addSource('overlayDataSource', overlayDataSource);
-                map.addLayer(overlayData);
-                getOverlayFeatures();
+        map.addSource('overlayDataSource', overlayDataSource);
+        map.addLayer(overlayData);
+        getOverlayFeatures();
 
-                map.on('click', function(e) {
+        map.on('click', function(e) {
 
-                    // Add review marker
-                    var newOverlayFeature = {
-                        "type": "Feature",
-                        "properties": {},
-                        "geometry": {
-                            "coordinates": [
+            // Add review marker
+            var newOverlayFeature = {
+                "type": "Feature",
+                "properties": {},
+                "geometry": {
+                    "coordinates": [
 
-                            ],
-                            "type": "Point"
-                        }
-                    };
-
-                    var clickedOverlayFeatures = map.queryRenderedFeatures([
-                        [e.point.x - 5, e.point.y - 5],
-                        [e.point.x + 5, e.point.y + 5]
-                    ], {
-                        layers: ['overlayData']
-                    });
-                    if (clickedOverlayFeatures.length) {
-                        overlayFeatureForm(clickedOverlayFeatures[0]);
-
-                    } else {
-                        overlayFeatureForm();
-                    }
-
-                    function overlayFeatureForm(feature) {
-                        var formOptions = "<div class='radio-pill pill pad2y clearfix' style='width:350px'><input id='valid' type='radio' name='review' value='valid' checked='checked'><label for='valid' class='col4 button short icon check fill-green'>Valid</label><input id='redundant' type='radio' name='review' value='redundant'><label for='redundant' class='col4 button short icon check fill-mustard'>Redundant</label><input id='invalid' type='radio' name='review' value='invalid'><label for='invalid' class='col4 button icon short check fill-red'>Invalid</label></div>";
-                        var formReviewer = "<fieldset><label>Reviewed by: <span id='reviewer' style='padding:5px;background-color:#eee'></span></label><input type='text' name='reviewer' placeholder='OSM username'></input></fieldset>"
-                        var popupHTML = "<h3>" + restriction + "</h3><form>" + formOptions + formReviewer + "<a id='updateOverlayFeature' class='button col4' href='#'>Save</a><a id='deleteOverlayFeature' class='button quiet fr col4' href='#' style=''>Delete</a></form>";
-                        var popup = new mapboxgl.Popup()
-                            .setLngLat(e.lngLat)
-                            .setHTML(popupHTML)
-                            .addTo(map);
-
-                        // Show existing status if available
-                        if (feature) {
-                            $("input[name=review][value=" + feature.properties["status"] + "]").prop('checked', true);
-                            $("#reviewer").html(feature.properties["reviewer"]);
-                            newOverlayFeature = feature;
-                            newOverlayFeature["id"] = feature.properties["id"];
-                            console.log(feature);
-                        } else {
-                            newOverlayFeature.properties["name"] = restriction;
-                            newOverlayFeature.geometry.coordinates = e.lngLat.toArray();
-                        }
-
-                        // Set reviewer name if previously saved
-                        if (reviewer) {
-                            $("input[name=reviewer]").val(reviewer);
-                        }
-
-                        // Update dataset with feature status on clicking save
-                        document.getElementById("updateOverlayFeature").onclick = function() {
-                            newOverlayFeature.properties["status"] = $("input[name=review]:checked").val();
-                            reviewer = $("input[name=reviewer]").val();
-                            newOverlayFeature.properties["contributed_by"] = reviewer;
-                            popup.remove();
-                            mapbox.insertFeature(newOverlayFeature, dataset, function(err, response) {
-                                console.log(response);
-                                overlayFeatureCollection.features = overlayFeatureCollection.features.concat(response);
-                                overlayDataSource.setData(overlayFeatureCollection);
-                            });
-                        };
-                        // Delete feature on clicking delete
-                        document.getElementById("deleteOverlayFeature").onclick = function() {
-                            popup.remove();
-                            mapbox.deleteFeature(newOverlayFeature["id"], dataset, function(err, response) {
-                                console.log(response);
-                            });
-                        };
-                    }
-
-                });
-
-
-            }
-
-
-
-            // Get data from a Mapbox dataset
-            var overlayFeatureCollection = {
-                'type': 'FeatureCollection',
-                'features': []
+                    ],
+                    "type": "Point"
+                }
             };
 
-            function getOverlayFeatures(startID) {
+            var clickedOverlayFeatures = map.queryRenderedFeatures([
+                [e.point.x - 5, e.point.y - 5],
+                [e.point.x + 5, e.point.y + 5]
+            ], {
+                layers: ['overlayData']
+            });
+            if (clickedOverlayFeatures.length) {
+                overlayFeatureForm(clickedOverlayFeatures[0]);
 
-                var url = DATASETS_BASE + 'features';
-                var params = {
-                    'access_token': mapboxAccessDatasetToken
-                };
+            } else {
+                overlayFeatureForm();
+            }
 
-                // Begin with the last feature of previous request
-                if (startID) {
-                    params.start = startID;
+            function overlayFeatureForm(feature) {
+                var formOptions = "<div class='radio-pill pill pad2y clearfix' style='width:350px'><input id='valid' type='radio' name='review' value='valid' checked='checked'><label for='valid' class='col4 button short icon check fill-green'>Valid</label><input id='redundant' type='radio' name='review' value='redundant'><label for='redundant' class='col4 button short icon check fill-mustard'>Redundant</label><input id='invalid' type='radio' name='review' value='invalid'><label for='invalid' class='col4 button icon short check fill-red'>Invalid</label></div>";
+                var formReviewer = "<fieldset><label>Reviewed by: <span id='reviewer' style='padding:5px;background-color:#eee'></span></label><input type='text' name='reviewer' placeholder='OSM username'></input></fieldset>"
+                var popupHTML = "<h3>" + restriction + "</h3><form>" + formOptions + formReviewer + "<a id='updateOverlayFeature' class='button col4' href='#'>Save</a><a id='deleteOverlayFeature' class='button quiet fr col4' href='#' style=''>Delete</a></form>";
+                var popup = new mapboxgl.Popup()
+                    .setLngLat(e.lngLat)
+                    .setHTML(popupHTML)
+                    .addTo(map);
+
+                // Show existing status if available
+                if (feature) {
+                    $("input[name=review][value=" + feature.properties["status"] + "]").prop('checked', true);
+                    $("#reviewer").html(feature.properties["reviewer"]);
+                    newOverlayFeature = feature;
+                    newOverlayFeature["id"] = feature.properties["id"];
+                    console.log(feature);
+                } else {
+                    newOverlayFeature.properties["name"] = restriction;
+                    newOverlayFeature.geometry.coordinates = e.lngLat.toArray();
                 }
 
-                $.getJSON(url, params, function(data) {
+                // Set reviewer name if previously saved
+                if (reviewer) {
+                    $("input[name=reviewer]").val(reviewer);
+                }
 
-                    console.log(data);
-
-                    if (data.features.length) {
-                        data.features.forEach(function(feature) {
-                            // Add dataset feature id as a property
-                            feature.properties.id = feature.id;
-                        });
-                        overlayFeatureCollection.features = overlayFeatureCollection.features.concat(data.features);
-                        var lastFeatureID = data.features[data.features.length - 1].id;
-                        getOverlayFeatures(lastFeatureID);
+                // Update dataset with feature status on clicking save
+                document.getElementById("updateOverlayFeature").onclick = function() {
+                    newOverlayFeature.properties["status"] = $("input[name=review]:checked").val();
+                    reviewer = $("input[name=reviewer]").val();
+                    newOverlayFeature.properties["contributed_by"] = reviewer;
+                    popup.remove();
+                    mapbox.insertFeature(newOverlayFeature, dataset, function(err, response) {
+                        console.log(response);
+                        overlayFeatureCollection.features = overlayFeatureCollection.features.concat(response);
                         overlayDataSource.setData(overlayFeatureCollection);
-                    }
-                    overlayDataSource.setData(overlayFeatureCollection);
+                    });
+                };
+                // Delete feature on clicking delete
+                document.getElementById("deleteOverlayFeature").onclick = function() {
+                    popup.remove();
+                    mapbox.deleteFeature(newOverlayFeature["id"], dataset, function(err, response) {
+                        console.log(response);
+                    });
+                };
+            }
+
+        });
+
+
+    }
+
+
+
+    // Get data from a Mapbox dataset
+    var overlayFeatureCollection = {
+        'type': 'FeatureCollection',
+        'features': []
+    };
+
+    function getOverlayFeatures(startID) {
+
+        var url = DATASETS_BASE + 'features';
+        var params = {
+            'access_token': mapboxAccessDatasetToken
+        };
+
+        // Begin with the last feature of previous request
+        if (startID) {
+            params.start = startID;
+        }
+
+        $.getJSON(url, params, function(data) {
+
+            console.log(data);
+
+            if (data.features.length) {
+                data.features.forEach(function(feature) {
+                    // Add dataset feature id as a property
+                    feature.properties.id = feature.id;
                 });
+                overlayFeatureCollection.features = overlayFeatureCollection.features.concat(data.features);
+                var lastFeatureID = data.features[data.features.length - 1].id;
+                getOverlayFeatures(lastFeatureID);
+                overlayDataSource.setData(overlayFeatureCollection);
+            }
+            overlayDataSource.setData(overlayFeatureCollection);
+        });
+    }
+
+});
+
+// Toggle visibility of a layer
+function toggle(id) {
+    var currentState = map.getLayoutProperty(id, 'visibility');
+    var nextState = currentState === 'none' ? 'visible' : 'none';
+    map.setLayoutProperty(id, 'visibility', nextState);
+}
+
+// Toggle a set of filters for a set of layers
+function toggleLayerFilters(layerItems, filterItem) {
+
+    for (var i in layerItems) {
+        for (var j in toggleLayers[layerItems[i]].layers) {
+
+            var existingFilter = map.getFilter(toggleLayers[layerItems[i]].layers[j]);
+
+            // Construct and add the filters if none exist for the layers
+            if (typeof existingFilter == 'undefined') {
+                map.setFilter(toggleLayers[layerItems[i]].layers[j], toggleFilters[filterItem].filter);
+            } else {
+                // Not implemented
+                var newFilter = mergeLayerFilters(existingFilter, toggleFilters[filterItem].filter);
+                map.setFilter(toggleLayers[layerItems[i]].layers[j], newFilter);
+                // console.log(newFilter);
             }
 
-            // Toggle visibility of a layer
-            function toggle(id) {
-                var currentState = map.getLayoutProperty(id, 'visibility');
-                var nextState = currentState === 'none' ? 'visible' : 'none';
-                map.setLayoutProperty(id, 'visibility', nextState);
-            }
-
-            // Toggle a set of filters for a set of layers
-            function toggleLayerFilters(layerItems, filterItem) {
-
-                for (var i in layerItems) {
-                    for (var j in toggleLayers[layerItems[i]].layers) {
-
-                        var existingFilter = map.getFilter(toggleLayers[layerItems[i]].layers[j]);
-
-                        // Construct and add the filters if none exist for the layers
-                        if (typeof existingFilter == 'undefined') {
-                            map.setFilter(toggleLayers[layerItems[i]].layers[j], toggleFilters[filterItem].filter);
-                        } else {
-                            // Not implemented
-                            var newFilter = mergeLayerFilters(existingFilter, toggleFilters[filterItem].filter);
-                            map.setFilter(toggleLayers[layerItems[i]].layers[j], newFilter);
-                            // console.log(newFilter);
-                        }
-
-                    }
-                }
-            }
+        }
+    }
+}
